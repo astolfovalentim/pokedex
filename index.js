98643c052d1b8f66ae2811a32b6deaccf2e2721c
astolfovalentim <astolfom@gmail.com>
2022-03-22 15:03:25 -0300

    ok

--- index.js
+++ index.js
@@ -38,6 +38,11 @@ app.get("/", (req, res) => {
   res.render("index", { pokedex });
 });
 
+app.post("/add", (req, res) =>) {
+
+    
+}
+
 app.listen(3000, () =>
   console.log("Servidor rodando em http:// localhost:3000")
 );
